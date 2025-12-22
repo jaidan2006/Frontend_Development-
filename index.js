eb3a6a987c88458fc9f54c67cd8edfbc624be4a3
jaidan2006 <jaidanray@gmail.com>
2025-12-22 10:46:01 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+import readline from "readline";
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+//Ex 1: Swap two numbers without using a third variable.
+// let a=10
+// let b=20
+// console.log("Before Swap: "+a+" and "+b)
+// a=a+b
+// b=a-b
+// a=a-b
+// console.log("After Swap: "+a+" and "+b)
+
+//Ex 2: Calculate the simple interest (SI = P × R × T / 100).
